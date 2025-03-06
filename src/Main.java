21d53b765bc7e77cbde069f7ebc3a1ec7278c290
selinbsa <selinbasakagan@gmail.com>
2025-03-06 22:58:23 +0300

    Full commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Manava Hoşgeldiniz! Hangi meyve ve sebzelerden kaçar kilo istediğinizin bilgilerini girin lütfen.");
+
+        double pear = 2.14;
+        double apple = 3.67;
+        double tomato = 1.11;
+        double banana = 0.95;
+        double aurbergine = 5.00;
+
+        System.out.println("Armut kaç kilo? : ");
+        double kilo_pear = scanner.nextDouble();
+
+        System.out.println("Elma kaç kilo? : ");
+        double kilo_apple = scanner.nextDouble();
+
+        System.out.println("Domates kaç kilo? : ");
+        double kilo_tomato = scanner.nextDouble();
+
+        System.out.println("Muz kaç kilo? : ");
+        double kilo_banana = scanner.nextDouble();
+
+        System.out.println("Patlıcan kaç kilo? : ");
+        double kilo_aurbergine = scanner.nextDouble();
+
+        double total_price = (pear*kilo_pear)+(apple*kilo_apple)+(tomato*kilo_tomato)+(banana*kilo_banana)+(aurbergine*kilo_aurbergine);
+        System.out.println("Toplam tutar : "+ total_price + "TL");
+
+
+    }
+}
